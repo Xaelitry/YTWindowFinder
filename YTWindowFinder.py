835cff06145257d29c0c2669d5bd37ba94289d52
Xaelitry <14274548+Xaelitry@users.noreply.github.com>
2020-08-06 04:03:51 +1000

    Add files via upload

---
+++ YTWindowFinder.py
@@ -0,0 +1,14 @@
+import win32gui
+from time import sleep
+def winEnumHandler(hwnd, ctx):
+    if win32gui.IsWindowVisible(hwnd):
+        a = ''.join(win32gui.GetWindowText(hwnd))
+        if "YouTube" in a:
+            a = str(a).replace(" - YouTube - Mozilla Firefox","").replace(" - YouTube - Google Chrome","")
+            print(a)
+            spaced = a+"     "
+            with open("music.txt","w") as f: f.write(spaced)
+
+while True:
+    win32gui.EnumWindows(winEnumHandler, None)
+    sleep(5)
